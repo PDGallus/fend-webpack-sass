3d4c10b48d4df86d135abef8315385761fdc54e4
Philip Hahn <phahn@brockhaus-ag.de>
2021-04-27 20:19:53 +0200

    api challenge finished

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,19 +1,21 @@
-function handleSubmit(event) {
+async function handleSubmit(event) {
     event.preventDefault()
 
     // check what text was put into the form field
     let formText = document.getElementById('name').value
 
     myLib.checkForName(formText)
-    myLib.fetchWeather()
+    console.log();
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
+    myLib.fetchWeather()
     .then(res => {
+        console.log('res', res);
         return res.json()
     })
     .then(function(data) {
-        document.getElementById('results').innerHTML = data.message
+        console.log('data', data);
+        document.getElementById('results').textContent = data.main.temp;
     })
 }
 
--- src/client/js/weatherChecker.js
+++ src/client/js/weatherChecker.js
@@ -1,8 +1,9 @@
-async function fetchWeather () {
+function fetchWeather () {
+    console.log('::: Running fetch weather :::');
     const weatherBaseUrl = 'https://api.openweathermap.org/data/2.5/weather?zip=';
     const apiKey = '&appid=8001878202185efdb8d070fe525b7efa'; // <== add own apiKey here
     const units = '&units=metric';
-    return await fetch(weatherBaseUrl + '59077,de' + units + apiKey);
+    return fetch(weatherBaseUrl + '59077' + units + apiKey);
 }
 
 export { fetchWeather }
